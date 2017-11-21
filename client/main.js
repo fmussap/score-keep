4456a802c559652da39100f3125a92ec1c838034
fmussap <fabiomussap@yahoo.com>
2017-11-21 08:29:07 +0000

    update 21/11

--- client/main.js
+++ client/main.js
@@ -1,22 +1,84 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react'
+import ReactDom from 'react-dom'
+import { Meteor } from 'meteor/meteor'
+import { Tracker } from 'meteor/tracker'
 
-import './main.html';
+import { Players } from '../imports/api/players'
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+const renderPlayers = (players) => {
+  return players.map((player) => {
+    const handleRemove = () => {
+      Players.remove({
+        _id: player._id
+      })
+    }
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+    const handleAdd = () => {
+      Players.update(
+        {
+          _id: player._id
+        },
+        {
+          $set: {
+            score: player.score + 1
+          }
+        }
+      )
+    }
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+    const handleSub = () => {
+      Players.update(
+        {
+          _id: player._id
+        },
+        {
+          $set: {
+            score: player.score - 1
+          }
+        }
+      )
+    }
+
+    return (
+      <div
+        key={player._id}
+      >
+        {player.name} score: {player.score}
+        <button onClick={handleAdd}>+</button>
+        <button onClick={handleSub}>-</button>
+        <button onClick={handleRemove}>X</button>
+      </div>
+    )
+  })
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  const playerName = e.target.playerName.value
+
+  if (playerName) {
+    Players.insert({
+      name: playerName,
+      score: 0
+    })
+  }
+  e.target.playerName.value = ''
+}
+
+Meteor.startup(() => {
+  const title = 'Score Keep'
+  Tracker.autorun(() => {
+    let players = Players.find().fetch()
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        {renderPlayers(players)}
+        <form onSubmit={handleSubmit}>
+          <input type='text' name='playerName' placeholder='player name' />
+          <button>Add player</button>
+        </form>
+      </div>
+    )
+    ReactDom.render(jsx, document.getElementById('app'))
+  })
+})
